98e51877a2494b73c3eb759ae5e500889aeff4af
Sara Broad <saraebroad@gmail.com>
2018-08-27 20:54:02 -0400

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Seventeen from "./pages/2017";
+// import Eighteen from "./pages/2018";
+// import Nineteen from "./pages/2019";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+<Router>
+<div>
+<Switch>
+  <Route></Route>
+  {/* <Route exact path="/" component={Books} /> */}
+</Switch>
+</div>
+</Router>
+)
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
+
+// const App = () => (
+//   <Router>
+//     <div>
+//       <Nav />
+//       <Switch>
+//         <Route exact path="/" component={Books} />
+//         <Route exact path="/books" component={Books} />
+//         <Route exact path="/books/:id" component={Detail} />
+//         <Route component={NoMatch} />
+//       </Switch>
+//     </div>
+//   </Router>
+// );
+
+// export default App;
---
+++ client/src/components/fiction/index.js
@@ -0,0 +1,3 @@
+export { default } from "./SigninBox";
+
+export { default } from "./"
