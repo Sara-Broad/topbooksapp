a4da2fba1cdfbb307451a488a68c284340f0276e
Sara Broad <saraebroad@gmail.com>
2018-08-28 15:11:10 -0400

    add content

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Seventeen from "./pages/2017";
-// import Eighteen from "./pages/2018";
+import Eighteen from "./pages/2018";
 // import Nineteen from "./pages/2019";
 
 const App = () => (
   <Router>
     <div>
       <Switch>
-        {/* <Route exact path="/" component={Eighteen} /> */}
+        <Route exact path="/" component={Eighteen} />
         <Route exact path="/2017" component={Seventeen} />
       </Switch>
     </div>
