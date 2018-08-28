50d3ecc45cad1e170d94a18afefcb6a0dbeac5e0
Sara Broad <saraebroad@gmail.com>
2018-08-28 13:22:09 -0400

    Express-ness

--- server.js
+++ server.js
@@ -3,3 +3,27 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+
+// Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+})
